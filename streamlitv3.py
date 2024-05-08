b5b88a74c572ad80f6eddc8f0af3bbdebbf28fd6
geopedrov <168468300+geopedrov@users.noreply.github.com>
2024-05-08 15:40:31 -0300

    Update streamlitv3.py

--- streamlitv3.py
+++ streamlitv3.py
@@ -1,9 +1,11 @@
 import folium
 import streamlit as st
+from streamlit_folium import st_folium
 
 import streamlit as st
 import folium
 
 map = folium.Map(location=[-25.441105, -49.276855], zoom_start=12)
 
-st.write(map)
+st.header('Depois de muita dor de cabeça, o mapa.')
+st_folium(map, width=700)
